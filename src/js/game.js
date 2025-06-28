22ed9af87bfd86cdd2a6ec015a1d46258afeca6a
Fábio Veiga <veigabfabio@hotmail.com>
2025-06-28 16:52:05 -0300

    (teste) Troca de fases, remoção e criação das paths

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,58 @@
-class Game{
+class Game {
     static currentLevel = 0;
-    static iniciar(){
+    static iniciar() {
+        Game.setLevel(Game.currentLevel)
 
         Player.player.style.left = `${Player.x1}px`
         Player.player.style.top = `${Player.y1}px`
         var intervalo = setInterval(() => {
-            if (Binds.up == true || Binds.down == true || Binds.left == true || Binds.right == true){
+            if (Binds.up == true || Binds.down == true || Binds.left == true || Binds.right == true) {
                 Player.canPlayerMove()
             }
         }, 1);
+
+        document.querySelector("body").addEventListener("keydown", function (e) {
+            if (e.key == 'p') {
+                Game.currentLevel = Game.currentLevel + 1 <= Level.levels.length - 1 ? Game.currentLevel + 1 : Game.currentLevel
+
+                if (document.querySelectorAll(".path").length > 0) {
+                    for (let i = 0; i <= Level.levels.length - 1; i++) {
+                        if (Level.levels[i].id == Game.currentLevel) {
+                            Game.removeLevelPaths(Level.levels[i])
+                        }
+                    }
+                }
+                Game.setLevel(Game.currentLevel)
+            }
+            else if (e.key == 'o') {
+                Game.currentLevel = Game.currentLevel - 1 >= 0 ? Game.currentLevel - 1 : Game.currentLevel
+
+                if (document.querySelectorAll(".path").length > 0) {
+                    for (let i = 0; i <= Level.levels.length - 1; i++) {
+                        if (Level.levels[i].id == Game.currentLevel) {
+                            Game.removeLevelPaths(Level.levels[i])
+                        }
+                    }
+                }
+                Game.setLevel(Game.currentLevel)
+            }
+        })
+    }
+
+    static setLevel(level) {
+        for (let i = 0; i <= Level.levels.length - 1; i++) {
+            if (Level.levels[i].id == level) {
+                Game.createLevelPaths(Level.levels[i])
+            }
+        }
+    }
+
+    static createLevelPaths(level) {
+        level.createPaths()
+    }
+
+    static removeLevelPaths(level) {
+        level.removePaths()
     }
 }
 
--- src/js/level.js
+++ src/js/level.js
@@ -10,22 +10,34 @@ class Level {
 
         for (let i = 0; i <= paths.length - 1; i++) {
             this.paths.push(paths[i])
-            this.createPaths(paths[i])
+            // this.createPaths(paths[i])
         }
+
+        Level.levels.push(this)
     }
 
-    createPaths(path) {
+    createPaths() {
+        for (let i = 0; i <= this.paths.length - 1; i++) {
+            var path = this.paths[i]
+            var elementoPath = $(`<div class="path">${path.id}</div>`).css({
+                left: path.x1,
+                top: path.y1,
+                width: path.x2 - path.x1,
+                height: path.y2 - path.y1,
+                background: 'blue'
+            })
 
-        var elementoPath = $(`<div class="path">${path.id}</div>`).css({
-            left: path.x1,
-            top: path.y1,
-            width: path.x2 - path.x1,
-            height: path.y2 - path.y1,
-            background: 'blue'
-        })
+            $(".level").append(elementoPath)
+        }
 
-        $(".level").append(elementoPath)
+    }
 
+    removePaths() {
+        console.log(this.paths)
+        var paths = document.querySelectorAll(".path")
+        for (let i = 0; i <= paths.length - 1; i++) {
+            paths[i].remove()
+        }
     }
 }
 
@@ -48,10 +60,15 @@ var level1 = new Level(
         // corredor esquerda (310x100)
         new Path(100, 310, 410, 410),
         // campo esquerda (600x100)
-        new Path(0, 110, 100, 630),
-        // Campo teste 
-        new Path(560, 110, 900, 240)
+        new Path(0, 110, 100, 630)
     ]
 )
 
-Level.levels.push(level1)
+var level2 = new Level(
+    [
+        // Quadrado central (200x200)
+        new Path(410, 260, 610, 460)
+    ]
+)
+
+// Level.levels.push(level1)
